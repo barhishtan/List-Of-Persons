462cddc41c5cbbd2b04e7fff496082d22ac6e5ac
Artur Sokolov <barhishtan@gmail.com>
2019-08-10 15:45:01 +0200

    2. Added Controllers

---
+++ List Of Persons/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  List Of Persons
+//
+//  Created by Artur Sokolov on 10/08/2019.
+//  Copyright © 2019 Artur Sokolov. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
